8e23ce1d0bb991cdc60eed9f7e499eb5d3a29ea7
Kristján Oddsson <koddsson@gmail.com>
2023-02-15 14:45:03 +0100

    Just inject the script directly onto the page

--- web-dev-server-plugin-msw.js
+++ web-dev-server-plugin-msw.js
@@ -4,38 +4,13 @@ import path from 'node:path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const rootDir = process.cwd()
 
-// TODO: Clean this up a lot and work on the API. I'm thinking that it should have sane defaults.
-// {
-//    '/user': 'koddsson'
-// }
-//
-// Then it's possible to do more complicated things:
-//
-// {
-//   '/user': new Response('{"username": "koddsson"}),
-// }
-//
-// I wonder if we can make `Request` objects be the keys on the object.
-// 
-// {
-//   [new Request('/user', {method: 'POST'})]: new Response('{"username": "koddsson"}),
-// }
-//
-// Then matching requests might be super easy.
-export function mockPlugin(givenHandlers, options = {}) {
-  const {quiet = true} = options
+export function mockPlugin(filename, options = {}) {
+  const script = `<script type="module">
+    import '${filename}';
+  </script>`
 
-  const handlers = []
-
-  givenHandlers.forEach(async (response, request) => {
-    const body = await response.text()
-    const pathname = new URL(request.url).pathname
-    const verb = ['GET', 'POST'].includes(request.method) ? request.method.toLowerCase() : 'get'
-    handlers.push(`rest.${verb}('${pathname}', (req, res, ctx) => {
-      return res(ctx.status(${response.status || 200}), ctx.set(${JSON.stringify(Object.fromEntries(response.headers))}), ctx.body(\`${body}\`))
-    })`)
-  })
   return {
     name: 'ing-mocks',
     serve(context) {
@@ -49,20 +24,7 @@ export function mockPlugin(givenHandlers, options = {}) {
         return {
           body: context.body.replace(
             /<\/head>/,
-            `<script type="module">
-                import { rest, setupWorker } from '@bundled-es-modules/msw';
-
-                const handlers = [${await Promise.all(handlers)}];
-                const worker = setupWorker(...handlers);
-
-                worker.start({
-                  quiet: ${quiet},
-                  // See https://github.com/mswjs/msw/discussions/1231#discussioncomment-2729999 if you'd like to warn if there's a unhandled request
-                  onUnhandledRequest() {
-                    return undefined;
-                  },
-                });
-              </script>
+            `${script}
             </head>`,
           ),
         };

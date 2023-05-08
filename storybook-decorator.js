c2394c9506cd25565ce211aaf307aedab780254c
Kristján Oddsson <koddsson@gmail.com>
2023-05-08 16:09:42 +0200

    Add a storybook decorator

---
+++ storybook-decorator.js
@@ -0,0 +1,32 @@
+import { makeDecorator } from '@web/storybook-prebuilt/addons';
+import { rest, HttpResponse, worker} from './msw.js';
+
+export function withMocks(mocks) {
+  // Set up basic routes for convenience if they exists
+  const defaultRoutes = mocks?.default?.({ rest, HttpResponse }) || [];
+  if (defaultRoutes) {
+    worker.use(...defaultRoutes);
+  }
+  return makeDecorator({
+    name: 'withMocks',
+    parameterName: 'mocks',
+    wrapper: (getStory, context) => {
+      if (context.parameters.mocks) {
+        const routes = context.parameters.mocks({
+          rest,
+          HttpResponse,
+          worker,
+        });
+        if (routes) {
+          worker.resetHandlers(...routes, ...defaultRoutes);
+        }
+      } else {
+        worker.resetHandlers(...defaultRoutes);
+      }
+
+      return getStory(context);
+    },
+  });
+}
+
+export { rest, HttpResponse, worker };

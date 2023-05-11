26c4564faa4d6ebcc0ce8d1ce137ffe1cbb848cf
Kristján Oddsson <koddsson@gmail.com>
2023-05-11 14:21:09 +0200

    Add a rollup plugin

---
+++ rollup-plugin.js
@@ -0,0 +1,10 @@
+export default function mswRollupPlugin() {
+  return {
+    name: 'rollup-plugin-msw',
+    writeBundle(opts) {
+      const sw = fs.readFileSync('./mockServiceWorker.js', 'utf8');
+      const outPath = path.join(opts.dir, '__msw_sw__.js');
+      fs.writeFileSync(outPath, sw);
+    }
+  }
+}

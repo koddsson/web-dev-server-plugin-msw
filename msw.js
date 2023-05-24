c59dccd4e2b8babba124f6ae9d09f648663fd3ea
Kristján Oddsson <koddsson@gmail.com>
2023-05-24 11:09:43 +0200

    Simplify by allowing a arbitrary number of route callbacks

--- msw.js
+++ msw.js
@@ -1,10 +1,10 @@
-import { rest, HttpResponse } from 'msw';
-import { setupWorker } from 'msw/browser';
+import { rest, HttpResponse } from "msw";
+import { setupWorker } from "msw/browser";
 
-const worker = setupWorker()
+const worker = setupWorker();
 worker.start({
   serviceWorker: {
-    url: '__msw_sw__.js',
+    url: "__msw_sw__.js",
   },
   quiet: true,
   // See https://github.com/mswjs/msw/discussions/1231#discussioncomment-2729999 if you'd like to warn if there's a unhandled request
@@ -16,24 +16,15 @@ worker.start({
 /**
  * Set up mocking routes
  *
- * @param {(rest, response, worker) => unknown[]} mocks Callback that returns the MSW mocks.
- * @param {unknown[]} defaultRoutes The default routes
+ * @param {Array<(rest, response, worker) => unknown[]>} mocks Callback that returns the MSW mocks.
  */
-function setRoutes(mocks, defaultRoutesCallback) {
-  // Set up basic routes for convenience if they exists
-  const defaultRoutes = defaultRoutesCallback?.({rest, HttpResponse, worker}) || [];
-  if (!mocks) {
-  worker.use(...defaultRoutes);
-  } else {
-    const routes = mocks({
-      rest,
-      HttpResponse,
-      worker,
-    });
-    if (routes) {
-      worker.resetHandlers(...routes, ...defaultRoutes);
-    }
+function setRoutes(...routeCallbacks) {
+  const routes = []
+  for (const callback of routeCallbacks) {
+    routes.push(...(callback?.({ rest, HttpResponse, worker }) || []));
   }
+
+  worker.resetHandlers(...routes);
 }
 
-export {worker, rest, HttpResponse, setRoutes}
+export { worker, rest, HttpResponse, setRoutes };

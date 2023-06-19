f6298459eb386085de3401fd72ec32f5146ae7f1
Kristján Oddsson <koddsson@gmail.com>
2023-06-19 10:26:53 +0200

    Fix reading of mock service worker file

--- rollup-plugin.js
+++ rollup-plugin.js
@@ -1,11 +1,14 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import * as url from 'node:url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export function mswRollupPlugin() {
   return {
     name: 'rollup-plugin-msw',
     writeBundle(opts) {
-      const sw = fs.readFileSync('./mockServiceWorker.js', 'utf8');
+      const sw = fs.readFileSync(path.join(__dirname, './mockServiceWorker.js'), 'utf8');
       const outPath = path.join(opts.dir, '__msw_sw__.js');
       fs.writeFileSync(outPath, sw);
     }
